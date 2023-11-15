7b8aeb84f2ea1e03129c7fc632f30249b5821742
The WiseMan <thewiseman003@gmail.com>
2023-11-15 10:57:54 +0500

    Fixed to use single if instead of triple

--- src/api/movie/get.py
+++ src/api/movie/get.py
@@ -1,14 +1,11 @@
 from src.apps.movie.schemas.movie import MovieDetailSchema, IdMovieSchema
 from src.apps.movie.services.crud import crud_instance_movie
-from ninja.errors import ValidationError
 
 response = MovieDetailSchema
 
 
 async def handler(request, payload: IdMovieSchema):
     movie = await crud_instance_movie.read(payload.id)
-    if not movie:
-        raise ValidationError("Movie does not exist")
     
     movie = await crud_instance_movie.read(payload.id, related_fields=["directors", "actors", "genres"])
     
--- src/api/movie/patch.py
+++ src/api/movie/patch.py
@@ -1,22 +1,33 @@
 from src.apps.movie.schemas.movie import MoviePatchSchema, MovieDetailSchema
 from src.apps.movie.services.crud import crud_instance_movie
-from ninja.errors import ValidationError
 
 response = MovieDetailSchema
+
+# async def handler(request, payload: MoviePatchSchema):
+#     movie = await crud_instance_movie.read(payload.id)
+
+#     if not movie:
+#         raise ValidationError("Movie does not exist")
+
+#     data = payload.dict(exclude_unset=True)
+#     if 'actors' in data:
+#         await movie.actors.aset(data.pop('actors'))
+#     if 'directors' in data:
+#         await movie.directors.aset(data.pop('directors'))
+#     if 'genres' in data:
+#         await movie.genres.aset(data.pop('genres'))
+#     updated_instance = await crud_instance_movie.update(movie, data)
+#     instance = await crud_instance_movie.read(updated_instance.id, related_fields=["directors", "actors", "genres"])
+#     return instance
 
 async def handler(request, payload: MoviePatchSchema):
     movie = await crud_instance_movie.read(payload.id)
 
-    if not movie:
-        raise ValidationError("Movie does not exist")
-
     data = payload.dict(exclude_unset=True)
-    if 'actors' in data:
-        await movie.actors.aset(data.pop('actors'))
-    if 'directors' in data:
-        await movie.directors.aset(data.pop('directors'))
-    if 'genres' in data:
-        await movie.genres.aset(data.pop('genres'))
+    related_fields = ['actors', 'directors', 'genres']
+    for field in related_fields:
+        if field in data:
+            await getattr(movie, field).aset(data.pop(field))
     updated_instance = await crud_instance_movie.update(movie, data)
-    instance = await crud_instance_movie.read(updated_instance.id, related_fields=["directors", "actors", "genres"])
+    instance = await crud_instance_movie.read(updated_instance.id, related_fields=related_fields)
     return instance
